146beb84981c5982b030c0d084dd1ebceefdfa94
akashcstech <akashcstech@gmail.com>
2026-02-20 21:09:07 +0530

    Updated project with new changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const { initDb } = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173',"https://your-project.vercel.app"], credentials: true }));
 app.use(express.json());
 
 app.use('/api', authRoutes);
