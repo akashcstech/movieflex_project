b3af54941c0327b1836d7825633c4da230a8e14b
akashcstech <akashcstech@gmail.com>
2026-02-20 23:20:22 +0530

    Updated project with server.js cors to true

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const { initDb } = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: ['http://localhost:5173',"https://movieflex-project.vercel.app/"], credentials: true }));
+app.use(cors({ origin: true , credentials: true }));
 app.use(express.json());
 
 app.use('/api', authRoutes);
